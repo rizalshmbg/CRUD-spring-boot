2e21f2de0fc97e67aa00d15e0af88e329ca8ef09
rizalshmbg <sihombingrizal18@gmail.com>
2024-06-27 21:19:27 +0700

    Finish the RESTful API CRUD

---
+++ src/main/java/com/packt/cardatabase/web/CarController.java
@@ -0,0 +1,22 @@
+package com.packt.cardatabase.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+
+@RestController
+public class CarController {
+	private final CarRepository repository;
+	
+	public CarController(CarRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping("/cars")
+	public Iterable<Car> getCars() {
+		//fetch and return cars
+		return repository.findAll();
+	}
+}
